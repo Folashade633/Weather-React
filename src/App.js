90ff7f0a7f39294fe1829d00799c027ee38f4c33
Folashade <folajoachim@yahoo.co.uk>
2022-07-16 01:37:30 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <footer className="App-footer">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Folashade633/Vanilla-weather-app"
           target="_blank"
           rel="noopener noreferrer"
+          className="important"
         >
-          Learn React
+          Open-source code
         </a>
-      </header>
+        by Folashade Joachim
+      </footer>
     </div>
   );
 }
